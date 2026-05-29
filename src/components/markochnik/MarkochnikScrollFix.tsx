5f953d6eefd6945e64c60701d137280a99734d4f
SlyusarVA <slyusarva@gmail.com>
2026-05-29 18:49:05 +0700

    fix: mark body while markochnik is active

--- src/components/markochnik/MarkochnikScrollFix.tsx
+++ src/components/markochnik/MarkochnikScrollFix.tsx
@@ -4,32 +4,8 @@ import { useEffect } from 'react'
 
 export default function MarkochnikScrollFix() {
   useEffect(() => {
-    const html = document.documentElement
-    const body = document.body
-
-    const prevHtmlOverflow = html.style.overflow
-    const prevHtmlOverflowY = html.style.overflowY
-    const prevHtmlHeight = html.style.height
-    const prevBodyOverflow = body.style.overflow
-    const prevBodyOverflowY = body.style.overflowY
-    const prevBodyHeight = body.style.height
-
-    html.style.overflow = 'auto'
-    html.style.overflowY = 'auto'
-    html.style.height = 'auto'
-    body.style.overflow = 'auto'
-    body.style.overflowY = 'auto'
-    body.style.height = 'auto'
-    body.style.setProperty('-webkit-overflow-scrolling', 'touch')
-
-    return () => {
-      html.style.overflow = prevHtmlOverflow
-      html.style.overflowY = prevHtmlOverflowY
-      html.style.height = prevHtmlHeight
-      body.style.overflow = prevBodyOverflow
-      body.style.overflowY = prevBodyOverflowY
-      body.style.height = prevBodyHeight
-    }
+    document.body.classList.add('markochnik-scroll-page')
+    return () => document.body.classList.remove('markochnik-scroll-page')
   }, [])
 
   return null
